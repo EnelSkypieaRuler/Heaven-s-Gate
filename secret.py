b2fb8cf047db2feb6f5a45bfbfdc71b82dd56502
EnelSkypieaRuler <117940587+EnelSkypieaRuler@users.noreply.github.com>
2022-11-26 10:40:01 +0100

    Hiding the secret key
    Now my key is safe

--- secret.py
+++
@@ -1,8 +0,0 @@
-def _key():
-    key='barcvrpr'
-    aplpha_num="abcdefghijklmnopqrstuvwxyz"
-    output=""
-    for i in key:
-        output+=aplpha_num[(aplpha_num.find(i)+13)%26]
-    return output
-key=_key()
